6b72b505a3645c65aea9b564dd1ac817d61b73d9
Luis Exe <132916937+Sx-Onfroy@users.noreply.github.com>
2026-02-24 19:34:42 -0400

    Practica #5

---
+++ WinFormsApp5/WinFormsApp5/Form1.cs
@@ -0,0 +1,46 @@
+namespace WinFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(maskedTextBox1.Text, out int numero))
+            {
+                if (numero <= 1)
+                {
+                    MessageBox.Show("No es primo");
+                    return;
+                }
+
+                bool esPrimo = true;
+
+                for (int i = 2; i <= Math.Sqrt(numero); i++)
+                {
+                    if (numero % i == 0)
+                    {
+                        esPrimo = false;
+                        break;
+                    }
+                }
+
+                if (esPrimo)
+                {
+                    MessageBox.Show("Es primo");
+                }
+                else
+                {
+                    MessageBox.Show("No es primo");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingresa Aquí un numero Primo: ");
+            }
+        }
+    }
+}
